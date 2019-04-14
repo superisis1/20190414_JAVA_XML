9ea982183e78ffd0eeacd22bc0466a3abd3da005
Insung Jung <superisis@gmail.com>
2019-04-14 11:13:19 +0900

    JAVA 에서 findViewById로 XML 연결 - 멤버변수 생성 - onCreate에서 finfViewById 연결

--- app/src/main/java/kr/insungjung/a20190414_java_xml/MainActivity.java
+++ app/src/main/java/kr/insungjung/a20190414_java_xml/MainActivity.java
@@ -3,8 +3,15 @@ package kr.insungjung.a20190414_java_xml;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView contentTextView;
+    EditText inputEditText;
+    Button okBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +19,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Log.d("메인-onCreate테스트","정말 실행되나?"); // d - debug
-        Log.e("메인-e로그","어떻게 나오나?"); // e - error
+        Log.d("메인-onCreate테스트", "정말 실행되나?"); // d - debug
+        Log.e("메인-e로그", "어떻게 나오나?"); // e - error
+
+        contentTextView = findViewById(R.id.contentTextView);
+        inputEditText = findViewById(R.id.inputEditText);
+        okBtn = findViewById(R.id.okBtn);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
