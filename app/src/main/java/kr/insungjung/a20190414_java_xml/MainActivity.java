46e1f7ff1f4fb905c575f028bb5d690b3d494903
Insung Jung <superisis@gmail.com>
2019-04-14 11:48:05 +0900

    Toast.Length_Long

--- app/src/main/java/kr/insungjung/a20190414_java_xml/MainActivity.java
+++ app/src/main/java/kr/insungjung/a20190414_java_xml/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
                 contentTextView.setText(userInputStr);
 
                 Toast.makeText(MainActivity.this, "입력값이 반영되었습니다.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "두번째 토스트", Toast.LENGTH_LONG).show();
             }
         });
 
